7b3b0da178c5552adcb13791a778e8ad1133db7f
henrik <henrikvdv@gmail.com>
2022-08-10 12:25:46 +0200

    Line length

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from crypto2 import run_all
 
 selected_crypto = st.selectbox("currency", ["ETH", "BTC"])
 print("selected: {}".format(selected_crypto))
-fig, advice = run_all(is_load_online=True, crypto=selected_crypto)
+fig, advice = run_all(is_load_online=True, crypto_1=selected_crypto)
 st.plotly_chart(fig)
--- crypto2.py
+++ crypto2.py
@@ -9,17 +9,21 @@ def get_crypto_price_csv():
 
 def get_crypto_price(symbol, exchange, start_date=None):
     api_key = 'YOUR API KEY'
-    api_url = f'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={symbol}&market={exchange}&apikey={api_key}'
+    api_url = f'https://www.alphavantage.co/query?function=DIGITAL_' \
+              f'CURRENCY_DAILY&symbol={symbol}&market={exchange}&' \
+              f'apikey={api_key}'
     raw_df = requests.get(api_url).json()
     df = pd.DataFrame(raw_df['Time Series (Digital Currency Daily)']).T
     df = df.rename(
-        columns={'1a. open (USD)': 'open', '2a. high (USD)': 'high', '3a. low (USD)': 'low', '4a. close (USD)': 'close',
+        columns={'1a. open (USD)': 'open', '2a. high (USD)': 'high',
+                 '3a. low (USD)': 'low', '4a. close (USD)': 'close',
                  '5. volume': 'volume'})
     for i in df.columns:
         df[i] = df[i].astype(float)
     df.index = pd.to_datetime(df.index)
     df = df.iloc[::-1].drop(
-        ['1b. open (USD)', '2b. high (USD)', '3b. low (USD)', '4b. close (USD)', '6. market cap (USD)'], axis=1)
+        ['1b. open (USD)', '2b. high (USD)', '3b. low (USD)',
+         '4b. close (USD)', '6. market cap (USD)'], axis=1)
     if start_date:
         df = df[df.index >= start_date]
     return df
@@ -36,10 +40,12 @@ def give_advise(close_values, threshold_sell, threshold_buy):
     return advice
 
 
-def run_all(is_load_online: bool, crypto):
+def run_all(is_load_online: bool,
+            crypto_1: str):
     # get data
     if is_load_online:
-        btc = get_crypto_price(symbol=crypto, exchange='USD', start_date='2020-01-01')
+        btc = get_crypto_price(symbol=crypto_1, exchange='USD',
+                               start_date='2020-01-01')
     else:
         btc = get_crypto_price_csv()
 
