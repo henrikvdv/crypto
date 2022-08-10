0ab049fef27553e8b3b94b1b092107c6aca2c50a
henrik <henrikvdv@gmail.com>
2022-08-10 14:32:28 +0200

    Format code

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ symbols = list(pd.read_csv("crypto_symbols.csv", sep=";")["Symbol"])
 selected_crypto_1 = st.selectbox("currency 1", symbols, 0)
 selected_crypto_2 = st.selectbox("currency 2", symbols, 1)
 print("selected: {}, {}".format(selected_crypto_1, selected_crypto_2))
-fig, advice, fig_1, fig_2 = run_all(crypto_1=selected_crypto_1, crypto_2=selected_crypto_2)
+fig, advice, fig_1, fig_2 = run_all(
+    crypto_1=selected_crypto_1, crypto_2=selected_crypto_2
+)
 st.plotly_chart(fig)
 st.plotly_chart(fig_1)
 st.plotly_chart(fig_2)
--- crypto2.py
+++ crypto2.py
@@ -7,22 +7,36 @@ from cachetools import cached, TTLCache
 
 @cached(cache=TTLCache(maxsize=1024, ttl=600))
 def get_crypto_price(symbol, exchange, start_date=None):
-    api_key = 'YOUR API KEY'
-    api_url = f'https://www.alphavantage.co/query?function=DIGITAL_' \
-              f'CURRENCY_DAILY&symbol={symbol}&market={exchange}&' \
-              f'apikey={api_key}'
+    api_key = "YOUR API KEY"
+    api_url = (
+        f"https://www.alphavantage.co/query?function=DIGITAL_"
+        f"CURRENCY_DAILY&symbol={symbol}&market={exchange}&"
+        f"apikey={api_key}"
+    )
     raw_df = requests.get(api_url).json()
-    df = pd.DataFrame(raw_df['Time Series (Digital Currency Daily)']).T
+    df = pd.DataFrame(raw_df["Time Series (Digital Currency Daily)"]).T
     df = df.rename(
-        columns={'1a. open (USD)': 'open', '2a. high (USD)': 'high',
-                 '3a. low (USD)': 'low', '4a. close (USD)': 'close',
-                 '5. volume': 'volume'})
+        columns={
+            "1a. open (USD)": "open",
+            "2a. high (USD)": "high",
+            "3a. low (USD)": "low",
+            "4a. close (USD)": "close",
+            "5. volume": "volume",
+        }
+    )
     for i in df.columns:
         df[i] = df[i].astype(float)
     df.index = pd.to_datetime(df.index)
     df = df.iloc[::-1].drop(
-        ['1b. open (USD)', '2b. high (USD)', '3b. low (USD)',
-         '4b. close (USD)', '6. market cap (USD)'], axis=1)
+        [
+            "1b. open (USD)",
+            "2b. high (USD)",
+            "3b. low (USD)",
+            "4b. close (USD)",
+            "6. market cap (USD)",
+        ],
+        axis=1,
+    )
     if start_date:
         df = df[df.index >= start_date]
     return df
@@ -40,14 +54,14 @@ def give_advise(close_values, threshold_sell, threshold_buy):
 
 
 def run_all(crypto_1: str, crypto_2: str):
-    exchange = 'USD'
-    start_date = '2020-01-01'
-    data_1 = get_crypto_price(symbol=crypto_1,
-                              exchange=exchange,
-                              start_date=start_date)
-    data_2 = get_crypto_price(symbol=crypto_2,
-                              exchange=exchange,
-                              start_date=start_date)
+    exchange = "USD"
+    start_date = "2020-01-01"
+    data_1 = get_crypto_price(
+        symbol=crypto_1, exchange=exchange, start_date=start_date
+    )
+    data_2 = get_crypto_price(
+        symbol=crypto_2, exchange=exchange, start_date=start_date
+    )
 
     close_1 = list(data_1["close"].values)
     close_2 = list(data_2["close"].values)
@@ -67,13 +81,8 @@ def run_all(crypto_1: str, crypto_2: str):
 
 def plot_time_series(date, ratio, ratio_name):
     # plot
-    fig = px.scatter(
-        x=date,
-        y=ratio
-    )
+    fig = px.scatter(x=date, y=ratio)
     fig.update_layout(
-        title=ratio_name,
-        xaxis_title="Date",
-        yaxis_title=ratio_name
+        title=ratio_name, xaxis_title="Date", yaxis_title=ratio_name
     )
     return fig
