32d444caa37e5293c90efd5f0fe06d691ab47437
henrik <henrikvdv@gmail.com>
2022-08-11 12:07:07 +0200

    Use 2 columns for dropdown

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import streamlit as st
 from crypto2 import run_all
 
 symbols = list(pd.read_csv("crypto_symbols.csv", sep=";")["Symbol"])
-selected_crypto_1 = st.selectbox("currency 1", symbols, 0)
-selected_crypto_2 = st.selectbox("currency 2", symbols, 1)
+col1, col2 = st.columns(2)
+with col1:
+    selected_crypto_1 = st.selectbox("currency 1", symbols, 0)
+with col2:
+    selected_crypto_2 = st.selectbox("currency 2", symbols, 1)
 print("selected: {}, {}".format(selected_crypto_1, selected_crypto_2))
 fig, advice, fig_1, fig_2 = run_all(
     crypto_1=selected_crypto_1, crypto_2=selected_crypto_2
